526a1af0895a6ba5b4890717deb1cba69196d214
betokov <betokov93@mail.ru>
2020-06-27 19:32:02 +0300

    Сверстан подвал, почти сверстана шапка

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    alert(1);
+    $('.burger').on('click', function() {
+        $('.sub-menu-inner').slideToggle();
+        if($(this).attr('style')) {
+            $(this).removeAttr('style');
+        } else {
+            $(this).css('background-image', 'url(img/close.png)');
+        }
+    });
+
+    $( window ).resize(function() {
+        if(window.innerWidth >= 1280) {
+            if($('.sub-menu-inner').attr('style')) {
+                $('.sub-menu-inner').removeAttr('style');
+                $('.burger').removeAttr('style');
+            }
+        }
+    });
 });
