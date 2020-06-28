6806e7b1a26a5724b11c2507f9eef64cebf66e27
betokov <betokov93@mail.ru>
2020-06-28 18:49:46 +0300

    Доработана шапка, свертан блок карта и блок документы

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     });
 
     $( window ).resize(function() {
-        if(window.innerWidth >= 1280) {
+        if(window.innerWidth >= 1100) {
             if($('.sub-menu-inner').attr('style')) {
                 $('.sub-menu-inner').removeAttr('style');
                 $('.burger').removeAttr('style');
@@ -17,3 +17,49 @@ $(document).ready(function(){
         }
     });
 });
+
+var swiper = new Swiper('.documents__slider', {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 5,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 1.3,
+            spaceBetween: 42,
+        },
+        767: {
+            slidesPerView: 2.3,
+            spaceBetween: 42,
+        },
+        991: {
+            slidesPerView: 2.3,
+            spaceBetween: 42,
+        },
+        1100: {
+            slidesPerView: 3.3,
+            spaceBetween: 42,
+        },
+        1599: {
+            slidesPerView: 4.3,
+            spaceBetween: 42,
+        }
+    }
+});
+
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        controls: ['zoomControl', 'geolocationControl'],
+        zoom: 11
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+}
